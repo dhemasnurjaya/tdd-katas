4d314ac66d5b4794c06f4b318a73a234a11d558a
Dhemas Nurjaya <dhemasnurjaya@gmail.com>
2026-02-10 08:22:39 +0700

    GREEN: return hardcoded "IV" for 4

--- lib/roman_numerals.dart
+++ lib/roman_numerals.dart
@@ -3,7 +3,12 @@ String integerToRoman(int number) {
   for (int i = 0; i < number; i++) {
     if (number == 5) {
       result += 'V';
-      break;
+      return result;
+    }
+
+    if (number == 4) {
+      result += 'IV';
+      return result;
     }
 
     result += 'I';
