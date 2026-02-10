27596c86db525e299ad44757ab2a56cf5cf98f99
Dhemas Nurjaya <dhemasnurjaya@gmail.com>
2026-02-10 08:10:32 +0700

    RED: test converts 2 to II

--- test/roman_numerals_test.dart
+++ test/roman_numerals_test.dart
@@ -5,4 +5,8 @@ void main() {
   test('converts 1 to I', () {
     expect(roman_numerals.integerToRoman(1), 'I');
   });
+
+  test('converts 2 to II', () {
+    expect(roman_numerals.integerToRoman(2), 'II');
+  });
 }
