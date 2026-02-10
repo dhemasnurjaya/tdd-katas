becef97ad808f83727471a87803312e95d881c65
Dhemas Nurjaya <dhemasnurjaya@gmail.com>
2026-02-10 08:19:23 +0700

    GREEN: return hardcoded "V" for 5

--- lib/roman_numerals.dart
+++ lib/roman_numerals.dart
@@ -1,6 +1,11 @@
 String integerToRoman(int number) {
   var result = '';
   for (int i = 0; i < number; i++) {
+    if (number == 5) {
+      result += 'V';
+      break;
+    }
+
     result += 'I';
   }
   return result;
