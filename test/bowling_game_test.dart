844cf381e41dc0d60d8764c053430d7bdf32a2cb
Dhemas Nurjaya <dhemasnurjaya@gmail.com>
2026-02-10 11:37:52 +0700

    RED: test one spare

--- test/bowling_game_test.dart
+++ test/bowling_game_test.dart
@@ -24,5 +24,14 @@ void main() {
       rollMany(20, 1);
       expect(game.score(), 20);
     });
+
+    test('one spare', () {
+      game.roll(5);
+      game.roll(5); // spare
+      game.roll(3); // bonus for spare
+      rollMany(17, 0); // rest are gutter balls
+
+      expect(game.score(), 16); // 10 (spare) + 3 (bonus) + 3 (normal roll)
+    });
   });
 }
