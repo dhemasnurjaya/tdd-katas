97709fa4dc280e487cff3030af0c322884ffd95e
Dhemas Nurjaya <dhemasnurjaya@gmail.com>
2026-02-10 08:06:33 +0700

    RED: test convert 1 to I

--- test/roman_numerals_test.dart
+++ test/roman_numerals_test.dart
@@ -1,1 +1,8 @@
-void main() {}
+import 'package:tdd_katas/roman_numerals.dart' as roman_numerals;
+import 'package:test/test.dart';
+
+void main() {
+  test('converts 1 to I', () {
+    expect(roman_numerals.integerToRoman(1), 'I');
+  });
+}
